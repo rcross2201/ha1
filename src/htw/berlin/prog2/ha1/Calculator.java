72e80c953e922bf335ea64008e9cf9dda92edeaf
rcross2201 <rohat.seran@student.htw-berlin.de>
2025-04-11 17:51:55 +0200

    commit für Fix zum ersten roten Test

--- src/htw/berlin/prog2/ha1/Calculator.java
+++ src/htw/berlin/prog2/ha1/Calculator.java
@@ -84,7 +84,7 @@ public class Calculator {
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
-        if (screen.equals("NaN")) screen = "Error";
+        if (screen.equals("NaN") || screen.equals("Infinity")) screen = "Error";
         if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
 
     }
@@ -121,6 +121,7 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
+
         var result = switch (latestOperation) {
             case "+" -> latestValue + Double.parseDouble(screen);
             case "-" -> latestValue - Double.parseDouble(screen);
